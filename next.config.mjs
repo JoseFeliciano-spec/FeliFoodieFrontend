d6121489137c673a91cbd935393c0a8524b148ce
JoseFeliciano-spec <68482634+JoseFeliciano-spec@users.noreply.github.com>
2025-01-31 16:14:07 -0500

    fix: Quitar optimización por el cobro de vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: false,
     domains: [
       "maps.googleapis.com",
       "hebbkx1anhila5yf.public.blob.vercel-storage.com",
-      "lh3.googleusercontent.com"
+      "lh3.googleusercontent.com",
     ],
   },
 };
